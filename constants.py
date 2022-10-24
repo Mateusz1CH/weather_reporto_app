2e25d7a2602ae9ccfab49ecdaaef6f35e250ef79
Mateo <mateusz1cholewa@gmail.com>
2022-10-24 23:21:42 +0200

    file with constant values, lat/lon, as well as structure of some of scrapper data

---
+++ constants.py
@@ -0,0 +1,51 @@
+cities_coordinates = {'Krakow':{'lat': '50.0469432', 'lon' : '19.997153435836697'},
+                      'London':{'lat': '51.5073219', 'lon' : '-0.1276474'},
+                      'Amsterdam':{'lat': '52.3727598', 'lon' : '4.8936041'},
+                      'Dubai':{'lat': '25.2653471', 'lon' : '55.2924914'},
+                      'Tokio':{'lat': '35.6828387', 'lon' : '139.7594549'},
+                      'Shanghai':{'lat': '31.2322758', 'lon' : '121.4692071'},
+                      'Delhi':{'lat': '28.6517178', 'lon' : '77.2219388'},
+                      'Cairo':{'lat': '30.0443879', 'lon' : '31.2357257'},
+                      'Karachi':{'lat': '24.8546842', 'lon' : '67.0207055'},
+                      'Istanbul':{'lat': '41.0091982', 'lon' : '28.9662187'},
+                      'Lagos':{'lat': '6.4550575', 'lon' : '3.3941795'},
+                      'Sao Paulo':{'lat': '-23.5506507', 'lon' : '-46.6333824'},
+                      'Mexico City':{'lat': '19.4326296', 'lon' : '-99.1331785'},
+                      'Mumbai':{'lat': '19.0785451', 'lon' : '72.878176'},
+                      'Beijing':{'lat': '39.906217', 'lon' : '116.3912757'},
+                      'Dhaka':{'lat': '23.7644025', 'lon' : '90.389015'},
+                      'New York':{'lat': '40.7127281', 'lon' : '-74.0060152'},
+                      'Buenos Aires':{'lat': '-34.6075682', 'lon' : '-58.4370894'},
+                      'Manila':{'lat': '14.5948914', 'lon' : '120.9782618'},
+                      'Kinshasa':{'lat': '-4.3217055', 'lon' : '15.3125974'},
+                      'Tianjin':{'lat': '39.0856735', 'lon' : '117.1951073'},
+                      'Los Angeles':{'lat': '34.0536909', 'lon' : '-118.242766'},
+                      'Moscow':{'lat': '55.7504461', 'lon' : '37.6174943'},
+                      'Paris':{'lat': '48.8588897', 'lon' : '2.3200410217200766'},
+                      'Bogota':{'lat': '4.6534649', 'lon' : '-74.0836453'},
+                      'Jakarta':{'lat': '-6.1753942', 'lon' : '106.827183'},
+                      'Lima':{'lat': '-12.0621065', 'lon' : '-77.0365256'},
+                      'Bangkok':{'lat': '13.7524938', 'lon' : '100.4935089'},
+                      'Seoul':{'lat': '37.5666791', 'lon' : '126.9782914'},
+                      'Tehran':{'lat': '35.6892523', 'lon' : '51.3896004'},
+                      'Kuala Lumpur':{'lat': '3.1516964', 'lon' : '101.6942371'},
+                      'Riyadh':{'lat': '24.638916', 'lon' : '46.7160104'},
+                      'Baghdad':{'lat': '33.3061701', 'lon' : '44.3872213'},
+                      'Santiago':{'lat': '-33.4377756', 'lon' : '-70.6504502'},
+                      'Madrid':{'lat': '40.4167047', 'lon' : '-3.7035825'},
+                      'Toronto':{'lat': '43.6534817', 'lon' : '-79.3839347'},
+                      'Dar es Salaam':{'lat': '-6.8160837', 'lon' : '39.2803583'},
+                      'Miami':{'lat': '25.7741728', 'lon' : '-80.19362'},
+                      'Singapore':{'lat': '1.2899175', 'lon' : '103.8519072'},
+                      'Johannesburg':{'lat': '-26.205', 'lon' : '28.049722'}}
+
+polution_quality = {'1' : 'Good', '2' : 'Fair', '3' : 'Moderate', '4' : 'Poor', '5' : 'Very Poor'}
+
+search_weatcher_data = ['id', 'main', 'description', 'icon']
+search_wind_data = ['speed', 'deg']
+search_main_data = ['temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity']
+search_sys_data = ['country', 'sunrise', 'sunset'] #'type', 'id',
+search_coord_data = ['lon', 'lat']
+search_cloud = ['all']
+#pollution_aqi = ['aqi']
+pollution_comp = ['co', 'no', 'no2', 'o3', 'so2', 'pm2_5', 'pm10', 'nh3']
